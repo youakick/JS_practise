6f6e489184c770b2df37362aa66af1d0e57164c1
VaMos <example@gmail.com>
2022-11-28 19:14:02 +0100

    ain

--- first/script.js
+++ first/script.js
@@ -1,4 +1,7 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+'use strict';
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,12 +10,44 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его",""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его","");
+/* for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a !='' && b !='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-console.log(personalMovieDB);
+console.log(personalMovieDB); */
+
+let i = 0;
+do { const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a !='' && b !='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    i++;
+}
+while (i < 2); 
+
+
+if (personalMovieDB.count < 10) {
+    console.log("too low number of films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("you classic watcher");
+} else if (personalMovieDB.count >= 30) {
+    console.log("you are movie fan");
+} else {
+    console.log('error');
+}
+
